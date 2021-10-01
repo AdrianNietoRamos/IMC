30b15207f07aeca948e0f58ae42caeafcff3b254
AdrianNietoRamos <adrianietoemfb@gmail.com>
2021-10-01 12:08:25 +0100

    Initial commit

---
+++ src/main/java/dad/maven/imc/Imc.java
@@ -0,0 +1,20 @@
+package dad.maven.imc;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Imc extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+
+	}
+
+	public static void main(String[] args) {
+
+		launch(args);
+
+	}
+
+}
